96f44adcad9ce80a45af1648e7b540e324784884
Cassandra Umeh <umehcassandra@gmail.com>
2022-07-28 20:24:16 +0000

    added clear button built reset function manipulated DOM node for clear button

--- app.mjs
+++ app.mjs
@@ -1,39 +1,52 @@
 function startApp() {
-    // Your entire app should not necessarily be coded inside this 
-    // single function (though there's no penalty for that), 
-    // so create and use/call additional functions from here
-  
-    // pls remove the below and make some magic in here!
-    // console.log('make magic in here!');
-  
-    // const header = document.querySelector('h2');
-    // if(header) {
-    //   header.textContent = 'make some magic here!!';
-    // }
-  };
-const redactBtn = document.getElementById("btn");
-let textArea = document.getElementById("text-area")
-let textsToRedact = document.getElementById("texts-to-redact");
+  // Your entire app should not necessarily be coded inside this 
+  // single function (though there's no penalty for that), 
+  // so create and use/call additional functions from here
 
-let displayOutput = document.getElementById ("display-output");
+  // pls remove the below and make some magic in here!
+  // console.log('make magic in here!');
+
+  // const header = document.querySelector('h2');
+  // if(header) {
+  //   header.textContent = 'make some magic here!!';
+  // }
+};
+
+
+let inputText = document.querySelector('#text-area')
+let inputRedact = document.querySelector('#texts-to-redact')
+const button = document.querySelector('#btn')
+const RedactText = document.querySelector('#display-output')
 let replaceChar = document.getElementById ("replace-char")
-let rexPattern = /^[a-zA-Z]/;
+let clear = document.getElementById ("clear")
+
+button.addEventListener('click', hideString)
+clear.addEventListener ('click', reset)
 
 
-
-redactBtn.addEventListener("click", function(){
-     console.log("clicked")
-});
-textArea.addEventListener("keyup", function(event){
-    let initialInput = event.target.value;
-    console.log(initialInput);
-});
-textsToRedact.addEventListener("keyup", function(event){
-   let newInput = event.target.value;
-   console.log(newInput);
-});
-
+function reset(){
+   inputText.value = '';
+    inputRedact.value = '';
+    RedactText.value = '';
+    replaceChar.value = '';
   
-  // ======= DO NOT EDIT ============== //
-  export default startApp;
-  // ======= EEND DO NOT EDIT ========= //
+}
+
+function hideString() {
+  let str = inputText.value;
+  let substr = inputRedact.value.split(' ');
+
+  substr.map(e => {
+    const strRegExp = new RegExp(e, 'gi');
+    const length = e.length
+    str = str.replace(strRegExp, `${`${replaceChar.value|| "*"}` .repeat(length)}`);
+  })
+  RedactText.innerText = str;
+ 
+}
+
+
+
+// ======= DO NOT EDIT ============== //
+export default startApp;
+  // ======= EEND DO NOT EDIT ========= //||
